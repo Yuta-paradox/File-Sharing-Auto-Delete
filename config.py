2a185fd6598d0f56cc251b2f3db1f50aefb31865
Roshan78m <153927319+Roshan78m@users.noreply.github.com>
2024-07-01 17:51:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6811318860"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "filesharebot")
-DB_NAME = os.environ.get("DATABASE_NAME", "mongodb+srv://rm8826453:yxn7ANeE43CKNScl@cluster0.dx2drch.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rm8826453:yxn7ANeE43CKNScl@cluster0.dx2drch.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharebot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001994477982"))
